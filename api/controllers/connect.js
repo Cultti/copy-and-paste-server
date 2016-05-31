2cd4063d6d9d0bd4b4f11a3c575534af54634865
cultti <cultti@aukko.net>
2016-05-31 05:48:36 +0000

    Add comments to controllers

--- api/controllers/connect.js
+++ api/controllers/connect.js
@@ -8,43 +8,54 @@ module.exports = {
 };
 
 function connectById(req, res) {
+    // Get the request ID
     var id = req.swagger.params.id.value;
+
+    // Try find connection
     findConnection(id, function(err, socketId) {
+        // If there is an error
         if (err) {
             return res.status(500).json({
                 msg: 'Server error'
             });
         }
-        else {
-            console.log(io);
-            if (!socketId || !io.sockets.connected[socketId]) {
-                return res.status(404).json({
-                    msg: 'ID not found'
-                });
-            }
 
-            var socket = io.sockets.connected[socketId];
-            socket.emit('api-conected', {
-                msg: 'You are now connected to api client'
+        // Check if the socket exists, if not return error
+        if (!socketId || !io.sockets.connected[socketId]) {
+            return res.status(404).json({
+                msg: 'ID not found'
             });
-
-            socket.on('api-connected-response', function(data) {
-                socket.removeAllListeners('api-connected-response');
-                clearTimeout(timeout);
-
-                res.json({
-                    msg: 'You are now connected!'
-                });
-            });
-
-            // Wait 10 seconds before timeout
-            var timeout = setTimeout(function() {
-                socket.removeAllListeners('api-connected-response');
-
-                return res.status(408).json({
-                    msg: 'Web-client did not respond in timely manner'
-                });
-            }, 10000);
         }
+
+        // Get the socket
+        var socket = io.sockets.connected[socketId];
+        // Send information about connect attemp to client
+        socket.emit('api-conected', {
+            msg: 'You are now connected to api client'
+        });
+
+        // Wait for response from client
+        socket.on('api-connected-response', function(data) {
+            // Remove the listener for response
+            socket.removeAllListeners('api-connected-response');
+            // Clear the timeout waiter
+            clearTimeout(timeout);
+            
+            // Send response
+            res.json({
+                msg: 'You are now connected!'
+            });
+        });
+
+        // Wait 10 seconds before timeout
+        var timeout = setTimeout(function() {
+            // Clear the listener
+            socket.removeAllListeners('api-connected-response');
+
+            // Return error about timeout
+            return res.status(408).json({
+                msg: 'Web-client did not respond in timely manner'
+            });
+        }, 10000);
     });
 }
--- api/controllers/send.js
+++ api/controllers/send.js
@@ -8,46 +8,62 @@ module.exports = {
 };
 
 function sendById(req, res) {
+    // Get the request ID
     var id = req.swagger.params.id.value;
+
+    // Try find connection
     findConnection(id, function(err, socketId) {
+        // If there is an error, return
         if (err) {
             return res.status(500).json({
                 msg: 'Server error'
             });
         }
-        else if (!req.body.data) {
+        
+        // If we are missing data, return error
+        if (!req.body.data) {
             return res.status(400).json({
                 msg: 'Message is missing!'
             });
         }
-        else {
-            if (!socketId || !io.sockets.connected[socketId]) {
-                return res.status(404).json({
-                    msg: 'ID not found'
-                });
-            }
 
-            var socket = io.sockets.connected[socketId];
-            socket.emit('msg', {
-                data: req.body.data
+        // If the socket is not found or does not exists
+        if (!socketId || !io.sockets.connected[socketId]) {
+            return res.status(404).json({
+                msg: 'ID not found'
             });
-
-            socket.on('msg-response', function(data) {
-                socket.removeAllListeners('msg-response');
-                clearTimeout(timeout);
-
-                res.json({
-                    msg: 'Message delivered succesfully'
-                });
-            });
-
-            var timeout = setTimeout(function() {
-                socket.removeAllListeners('msg-response');
-
-                return res.status(408).json({
-                    msg: 'Web-client did not respond in timely manner'
-                });
-            }, 10000);
         }
+
+        // Get the socket
+        var socket = io.sockets.connected[socketId];
+        
+        // Send message to the client
+        socket.emit('msg', {
+            data: req.body.data
+        });
+
+        // Wait for ack from client
+        socket.on('msg-response', function(data) {
+            // Clear the ack listener
+            socket.removeAllListeners('msg-response');
+            // Clear timeout
+            clearTimeout(timeout);
+
+            // Send response that message was delivered succesfully
+            res.json({
+                msg: 'Message delivered succesfully'
+            });
+        });
+
+        // Wait for timeout
+        var timeout = setTimeout(function() {
+            // Remove listener
+            socket.removeAllListeners('msg-response');
+
+            // Send error about timeout
+            return res.status(408).json({
+                msg: 'Web-client did not respond in timely manner'
+            });
+        }, 10000);
     });
 }
