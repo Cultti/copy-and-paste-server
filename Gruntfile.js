3e6b34a2f32ebfe158bf1a9b599a401e427b9622
cultti <cultti@aukko.net>
2016-06-02 08:17:46 +0000

    Make testing automatic with grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,46 @@
 module.exports = function(grunt) {
     // Load tasks
     grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-jscoverage');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-coveralls');
 
     grunt.initConfig({
         wiredep: {
             task: {
                 src: ['public/index.html']
             }
+        },
+        jscoverage: {
+            src: {
+                expand: true,
+                cwd: 'api/',
+                src: ['**/*.js'],
+                dest: 'api-cov',
+                ext: '.js'
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'mocha-lcov-reporter',
+                    captureFile: 'api-cov/output/mocha-lcov-reporter.out',
+                    quiet: false
+                },
+                src: ['test/**/*.js']
+            }
+        },
+        coveralls: {
+            options: {
+                force: false
+            },
+            test: {
+                src: 'api-cov/output/mocha-lcov-reporter.out'
+            }
         }
     });
 
     // Register tasks
     grunt.registerTask('default', ['wiredep']);
-
+    grunt.registerTask('test', ['jscoverage', 'mochaTest', 'coveralls']);
 };
